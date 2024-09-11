b4df9051bf2b0c236df4ad672633d58991ff34dc
Chathura <hirechathura@gmail.com>
2024-09-11 19:35:34 +1200

    updated the folder structure and code

--- app/src/main/java/com/example/kbandroidtechassessment/ui/main/MainActivity.kt
+++ app/src/main/java/com/example/kbandroidtechassessment/ui/main/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.example.kbandroidtechassessment
+package com.example.kbandroidtechassessment.ui.main
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
@@ -29,6 +29,8 @@ import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.TextUnit
 import androidx.compose.ui.unit.TextUnitType
 import androidx.compose.ui.unit.dp
+import com.example.kbandroidtechassessment.data.getTransactions
+import com.example.kbandroidtechassessment.models.Transaction
 import com.example.kbandroidtechassessment.ui.theme.KBAndroidTechAssessmentTheme
 
 class MainActivity : ComponentActivity() {
@@ -99,42 +101,3 @@ fun TransactionItem(transaction: Transaction) {
         )
     }
 }
-
-data class Transaction(
-    val date: String,
-    val description: String,
-    val amount: Double
-)
-
-private fun getTransactions() = listOf(
-    Transaction("2024-09-22", "Restaurant", -35.00),
-    Transaction("2024-09-24", "Car Repair", -150.00),
-    Transaction("2024-09-11", "Utilities", -150.00),
-    Transaction("2024-09-19", "Clothing Store", -100.00),
-    Transaction("2024-09-12", "Car Repair", -200.00),
-    Transaction("2024-09-13", "Book Purchase", -30.00),
-    Transaction("2024-09-14", "Electronics", -500.00),
-    Transaction("2024-09-17", "Groceries", -70.00),
-    Transaction("2024-09-26", "Electronics", -300.00),
-    Transaction("2024-09-18", "Gym Membership", -50.00),
-    Transaction("2024-09-01", "Coffee Shop", -15.00),
-    Transaction("2024-09-02", "Grocery Store", -75.00),
-    Transaction("2024-09-05", "Clothing Store", -120.00),
-    Transaction("2024-09-06", "Gym Membership", -50.00),
-    Transaction("2024-09-30", "Gym Membership", -50.00),
-    Transaction("2024-09-15", "Vacation", -1500.00),
-    Transaction("2024-09-07", "Movie Tickets", -30.00),
-    Transaction("2024-09-08", "Salary", 2500.00),
-    Transaction("2024-09-09", "Groceries", -80.00),
-    Transaction("2024-09-23", "Groceries", -90.00),
-    Transaction("2024-09-10", "Rent", -1200.00),
-    Transaction("2024-09-20", "Movie Tickets", -25.00),
-    Transaction("2024-09-21", "Gas Station", -55.00),
-    Transaction("2024-09-25", "Utilities", -120.00),
-    Transaction("2024-09-27", "Vacation", -1000.00),
-    Transaction("2024-09-28", "Restaurant", -45.00),
-    Transaction("2024-09-29", "Groceries", -85.00),
-    Transaction("2024-09-16", "Restaurant", -40.00),
-    Transaction("2024-09-03", "Restaurant", -35.00),
-    Transaction("2024-09-04", "Gas Station", -60.00),
-)
